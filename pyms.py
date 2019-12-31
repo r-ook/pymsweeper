0c16f05c1048df8efb6a83960652e4ea6e3acaef
idlehandle <34138772+idlehandle@users.noreply.github.com>
2019-12-31 17:34:31 -0500

    Update pyms.py
    Changes:
    1.) First click will always be safe
    2.) Some cosmetic changes
    3.) Added option to disable sound on multi-clear when flags are not matched.

--- pyms.py
+++ pyms.py
@@ -6,52 +6,66 @@ from time import time
 import tkinter as tk
 
 # This feels kinda useless to be honest, probably will remove.
-Dimension = namedtuple('Dimension', 'x y')
+# DIMENSION = namedtuple('DIMENSION', 'x y')
 
 # Mouse state constants
 MOUSE_LEFT = 2 ** 8
 MOUSE_MID = 2 ** 9
 MOUSE_RIGHT = 2 ** 10
-OKAY = '☺'
-DEAD = '☠'
-YEAH = '✌'
 
+STATUS = namedtuple('STATUS', 'icon fg bg')
+STATUS_OKAY = STATUS('☺', 'black', 'gold')
+STATUS_BOOM = STATUS('☠', 'white', 'red3')
+STATUS_YEAH = STATUS('✌', 'white', 'limegreen')
+# DEAD = '☠'
+# YEAH = '✌'
+
+MODE_CONFIG = namedtuple('MODE_CONFIG', 'x y rate amount')
 MODE = {
-    0: {'dimension': Dimension(9, 9), 'amount': 10},
-    1: {'dimension': Dimension(16, 16), 'amount': 40},
-    2: {'dimension': Dimension(30, 16), 'amount': 99}
+    0: MODE_CONFIG(8, 8, None, 10),
+    1: MODE_CONFIG(16, 16, None, 40),
+    2: MODE_CONFIG(30, 16, None, 99)
+    # 0: MODE_CONFIG(DIMENSION(9, 9), None, 10),
+    # 1: MODE_CONFIG(DIMENSION(16, 16), None, 40),
+    # 2: MODE_CONFIG(DIMENSION(30, 16), None, 99)
 }
 
-def prod(iterable):
-    result = 1
-    for x in iterable:
-        result *= x
-    return result
+# def prod(iterable):
+#     result = 1
+#     for x in iterable:
+#         result *= x
+#     return result
 
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
         # default_font = font.nametofont('TkDefaultFont')
         # default_font.configure(size=8)
-        self.title('PyMS')
+        self.title('Pysweeper')
         self.menubar = tk.Menu(self)
+        self.empty_image = tk.PhotoImage(width=1, height=1)
         self.var_mode = tk.IntVar()
-        self.var_mode.set(2)
+        self.var_mode.set(1)
+        self.var_sound = tk.BooleanVar()
+        self.var_sound.set(True)
         diff_menu = tk.Menu(self, tearoff=0)
-        for i, mode in enumerate(('Noob', 'Skilled', 'Pro')):
+        for i, mode in enumerate(('Fresh', 'Skilled', 'Pro')):
             diff_menu.add_radiobutton(
                 label=mode,
                 value=i,
                 variable=self.var_mode,
-                command=lambda x=i: self.build_field(**MODE.get(x))
+                command=lambda x=i: self.build_field(MODE.get(x))
             )
-        # diff_menu.add_radiobutton(label='Noob', command=lambda: self.build_field(**MODE.get(1)))
-        # diff_menu.add_radiobutton(label='Skilled', command=lambda: self.build_field(**MODE.get(2)))
-        # diff_menu.add_radiobutton(label='Pro', command=lambda: self.build_field(**MODE.get(3)))
+        option_menu = tk.Menu(self, tearoff=0)
+        option_menu.add_checkbutton(
+            label='Sound',
+            variable=self.var_sound
+        )
         self.menubar.add_cascade(label='Mode', menu=diff_menu)
+        self.menubar.add_cascade(label='Options', menu=option_menu)
         self.config(menu=self.menubar)
-        self.face = tk.StringVar()
-        self.face.set(OKAY)
+        # self.face = tk.StringVar()
+        # self.face.set(OKAY)
         self.timer = Timer(self)
         self.field = None
         self.build_status_bar()
@@ -62,12 +76,16 @@ class GUI(tk.Tk):
         self.lbl_timer = tk.Label(self.frm_timer, textvariable=self.timer.string)
         self.btn_main = tk.Button(
             self.frm_status,
-            textvariable=self.face,
-            command=lambda: self.build_field(**MODE.get(self.var_mode.get())),
+            image=self.empty_image,
+            command=lambda: self.build_field(MODE.get(self.var_mode.get())),
             font=('tkDefaultFont', 18, 'bold'),
-            bg='yellow',
-            anchor=tk.N
+            width=32,
+            height=32,
+            compound='c',
+            relief=tk.GROOVE,
+            anchor=tk.CENTER
         )
+        self.update_status(STATUS_OKAY)
         self.frm_IEDs = tk.LabelFrame(self.frm_status, text='IEDs:')
         self.lbl_IEDs = tk.Label(self.frm_IEDs, text='0')
         
@@ -77,20 +95,27 @@ class GUI(tk.Tk):
         self.frm_status.columnconfigure(index=1, weight=1)
         self.frm_status.columnconfigure(index=2, weight=3)
         self.frm_timer.grid(row=0, column=0, sticky=tk.NSEW)
-        self.btn_main.grid(row=0, column=1, sticky=tk.N)
+        self.btn_main.grid(row=0, column=1, sticky=tk.NS)
         self.frm_IEDs.grid(row=0, column=2, sticky=tk.NSEW)
         self.lbl_timer.pack()
         self.lbl_IEDs.pack()
 
-    def build_field(self, *args, **kwargs):
+    def build_field(self, mode:MODE_CONFIG):
         if not self.field is None:
             self.field.destroy()
-        self.field = Field(self, *args, **kwargs)
+        self.field = Field(self, mode)
         self.field.build()
         # self.var_timer.set(0)
         self.lbl_IEDs.config(textvariable=self.field.IED_current)
-        self.face.set(OKAY)
+        self.update_status(STATUS_OKAY)
         self.timer.start()
+
+    def update_status(self, status:STATUS):
+        self.btn_main.config(
+            text=status.icon,
+            fg=status.fg,
+            bg=status.bg
+        )
 
     # def timing(self):
     #     if not self.timed is None:
@@ -118,142 +143,91 @@ class Timer:
 
     def _update(self):
         self.to_string()
-        self._job = self.master.after(ms=1000, func=self._update)
+        if not self.master.field.is_over:
+            self._job = self.master.after(ms=1000, func=self._update)
 
-    def to_string(self, val=None):
-        if val:
-            current = val
-        else:
-            current = time() - self.start_time
+    def to_string(self):
+        # if val:
+        #     current = val
+        # else:
+        current = time() - self.start_time
         h, m, s = int(current // 360), int(current % 360 // 60), int(current % 60)
         self.string.set(f'{h:02}:{m:02}:{s:02}')
+
+    # def timetuple(self, precision=False):
+    #     if precision:
+    #         pass
+    #     else:
+    #         return int(current // 360), int(current % 360 // 60), int(current % 60)
     
     def stop(self):
         self.end_time = time() - self.start_time
         self.to_string()
         self.master.after_cancel(self._job)
-        return self.end_time
+        # return self.end_time
 
 
 class Field:
-    def __init__(self, master:tk.Toplevel, dimension:Dimension=Dimension(16, 16), rate:float=.15, amount=40):
+    def __init__(self, master:tk.Toplevel, mode:MODE_CONFIG=MODE.get(0)): # dimension:DIMENSION=DIMENSION(16, 16), rate:float=.15, amount=40):
         self.master = master
-        self.dimension = dimension
+        self.mode = mode
         self.frame = tk.Frame(master=self.master)
-        self.empty_image = tk.PhotoImage(width=1, height=1)
         self.is_over = False
-        if amount:
-            self.IED_count = amount
+        if self.mode.amount:
+            self.IED_count = self.mode.amount
         else:
-            self.IED_count = int(prod(dimension) * rate)
+            self.IED_count = int(self.mode.x * self.mode.y * self.mode.rate)
         self.IED_current = tk.IntVar()
         self.IED_current.set(self.IED_count)
-        self.IEDs = self.set_IEDs()
         self.map_cleared = 0
-        self.map_goal = dimension.x * dimension.y - self.IED_count
-        # self.map = self.create_map()
         self.map = {
-            (x, y): MapIED(self, (x, y), 1) if (x, y) in self.IEDs else MapElem(self, (x, y))
-            #MapElem(self, (x, y), 'X' if (x, y) in self.IEDs else None)
-            for x in range(dimension.x)
-            for y in range(dimension.y)
+            (x, y): MapElem(self, (x, y))
+            # MapIED(self, (x, y), 1) if (x, y) in self.IEDs else MapElem(self, (x, y))
+            # MapElem(self, (x, y), 'X' if (x, y) in self.IEDs else None)
+            for x in range(self.mode.x)
+            for y in range(self.mode.y)
         }
-        # self.map = FieldMap(self.frame, dimension, self.IEDs)
-        # self.cells = {
-        #     (x, y): Surprise(coord=(x, y), elem=self.map.get((x, y)) master=self.frame, width=3) 
-        #     for x in range(dimension.x)
-        #     for y in range(dimension.y)
-        # }
-        # self.IEDs = sample(self.cells.keys(), self.IED_count) 
-        # for IED in self.IEDs:
-        #     self.cells[IED].is_IED = True
+        self.IEDs = set()
+        self.map_goal = self.mode.x * self.mode.y - self.IED_count
 
     def build(self):
-        # for (x, y) in self.cells.keys():
-        #     lbl = tk.Label(self.frame, text='X' if (x, y) in self.IEDs else '')
-        #     lbl.grid(row=y, column=x, sticky=tk.NSEW)
-        #     lbl.lower()
-        # self.map.fill()
-        # for coord, cell in self.cells.items():
-        #     build(cell, coord)
         for elem in self.map.values():
             elem.build_surprise_box()
         self.frame.pack_propagate(False)
         self.frame.pack()
 
-    def set_IEDs(self):
-        IEDs = set()
-        while len(IEDs) < self.IED_count:
-            IEDs.add(
-                (randrange(self.dimension.x), randrange(self.dimension.y))
-            )
-        return IEDs
+    def set_IEDs(self, current_coord:tuple=None):
+        # self.IEDs = set()
+        while len(self.IEDs) < self.IED_count:
+            coord = (randrange(self.mode.x), randrange(self.mode.y))
+            if coord != current_coord:
+                self.IEDs.add(coord)
+        for IED in self.IEDs:
+            self.map.get(IED).is_IED = True
+        # return IEDs
 
     def oops(self):
         self.master.timer.stop()
-        self.master.face.set(DEAD)
+        self.master.update_status(STATUS_BOOM)
         self.is_over = True
         for IED in self.IEDs:
             self.map.get(IED).reveal()
-        messagebox.showwarning("It's over Anakin!", 'I have the highground.')
+        for elem in self.map.values():
+            if elem.flagged:
+                elem.box.check_false_flag()
+        messagebox.showwarning('Oh no!', 'You done goofed!')
 
     def check_clear(self):
         self.map_cleared += 1
         if self.map_cleared >= self.map_goal:
             self.master.timer.stop()
-            self.master.face.set(YEAH)
+            self.master.update_status(STATUS_YEAH)
             self.is_over = True
-            messagebox.showinfo("It's wonderful!", 'You did it!')
-
-
-    # def create_map(self):
-    #     field_map = dict()
-    #     def count_IED(coord):
-    #         nonlocal field_map
-    #         cx, cy = coord
-    #         # count = sum(adj == 'X' for adj in adjacents)
-    #         count = sum(field_map.get((rx, ry)) == 'X' for rx in range(cx-1, cx+2) for ry in range(cy-1, cy+2))
-    #         # colour = FieldMap.clue_colours.get(count, 'SystemButtonText')
-    #         return count    # MapElem(self.frame, coord, count)
-    
-    #     for coord in self.IEDs:
-    #         field_map[coord] = MapElem(self, coord, 'X')
-    #     for x in range(self.dimension.x):
-    #         for y in range(self.dimension.y):
-    #             # adjacents = [field_map.get((rx, ry)) for rx in range(cx-1, cx+2) for ry in range(cy-1, cy+2) if field_map.get((rx, ry))]
-    #             field_map.setdefault(
-    #                 (x, y), 
-    #                 MapElem(
-    #                     self,
-    #                     (x, y),
-    #                     count_IED((x, y))
-    #                 )
-    #             )
-    #     return field_map
-    
+            messagebox.showinfo('Subarashi!', 'You did it!')
+   
     def destroy(self):
         self.frame.destroy()
 
-
-# class FieldMap:
-    
-#     # Inner class to build the map with numbers
-#     def __init__(self, master:tk.Toplevel, dimension:Dimension, IEDs:list):
-#         self.master = master
-#         self.dimension = dimension
-
-
-
-
-#     def get(self, coord):
-#         return self.map.get(coord)
-
-    
-#     def fill(self):
-#         for coord, elem in self.map.items():
-#             lbl = tk.Label(self.master, text=str(elem.val) if elem.val else '', fg=elem.colour)
-#             build(lbl, coord)
-#             lbl.lower()
 
 class MapElem:
     clue_colours = {
@@ -273,12 +247,20 @@ class MapElem:
         self.frame.pack_propagate(False)
         self.val = val
         # self.colour = MapElem.clue_colours.get(val, 'SystemButtonText')
-        self.is_IED = True if val else False
+        # self.is_IED = True if val else False
         self.clue = None
         self.flagged = False
         self.revealed = False
         self.box = None
         self.lbl = None
+
+    @property
+    def is_IED(self):
+        return True if self.val else False
+    
+    @is_IED.setter
+    def is_IED(self, value):
+        self.val = 1 if value else 0
 
     def build(self, widget): # widget:tk.Widget):
         self.frame.grid(row=self.coord[1], column=self.coord[0], sticky=tk.NSEW)
@@ -299,31 +281,36 @@ class MapElem:
         mapper = self.field.map
         return (mapper.get((rx, ry)) for rx in range(cx-1, cx+2) for ry in range(cy-1, cy+2) if mapper.get((rx, ry)))
 
+    def create_actual(self):
+        self.lbl = tk.Label(
+            master=self.frame,
+            text='✹' if self.val else self.clue if self.clue else '',
+            fg=MapElem.clue_colours.get(self.clue, 'SystemButtonText'),
+            font=('tkDefaultFont', 10, 'bold')
+        )
+        self.lbl.bind('<ButtonRelease-1>', self._click)
+        self.lbl.bind('<ButtonRelease-3>', self._click)
+        self.lbl.pack()
+
     def reveal(self):
+        if self.field.map_cleared == 0:
+            self.field.set_IEDs(self.coord)
         if not self.revealed and not self.flagged:
-            if not self.is_IED:
-                self.count_adjacent_IEDs()
-            self.lbl = tk.Label(
-                master=self.frame,
-                text='✹' if self.val else self.clue if self.clue else '',
-                fg=MapElem.clue_colours.get(self.clue, 'SystemButtonText'),
-                font=('tkDefaultFont', 10, 'bold')
-            )
-            self.lbl.bind('<ButtonRelease-1>', self._click)
-            self.lbl.bind('<ButtonRelease-3>', self._click)
-            self.box.destroy()
-            self.build(self.lbl)
+            # if not self.is_IED:
             self.revealed = True
+            self.count_adjacent_IEDs()
+            self.create_actual()
+            self.box.destroy()
             if self.clue == 0:
                 for elem in self.adjacents():
                     elem.reveal()
             if not self.field.is_over:
                 if self.is_IED:
-                    self.lbl.config(fg='red')
+                    self.lbl.config(fg='red', text='✨')
                     self.field.oops()
                 else:
                     self.field.check_clear()
-            return self.lbl
+            # return self.lbl
 
     def __eq__(self, other):
         return self.val == other
@@ -336,30 +323,16 @@ class MapElem:
             if (evt.num == 1 and evt.state & MOUSE_RIGHT) or (evt.num == 3 and evt.state & MOUSE_LEFT):
                 self.both_release()
             elif evt.num == 1:
-                self.left_release(evt.state)
+                self.left_release()
             elif evt.num == 3:
-                self.right_release(evt.state)
+                self.right_release()
 
-    def left_release(self, mods=None):
+    def left_release(self):
         self.reveal()
-        # tk.Label(self.master, text='X' if self.is_IED else 'O').grid(row=self.y, column=self.x, sticky=tk.NSEW)
-
-            # for elem in self.field.map.values():
-            #     if elem.is_IED:
-            #         elem.reveal()
-            #     elif not elem.revealed:
-            #         print(elem.coord)
-            #         elem.box.config(state=tk.DISABLED)
-            # messagebox.showwarning('Uh oh...', 'Boom!')
-            # self.frame.config(state=tk.DISABLED)
     
-    def right_release(self, mods=None):
+    def right_release(self):
         if not self.revealed:
             self.flagged = self.box.flag()
-        # if mods & MOUSE_LEFT:
-        #     self.reveal()
-        #     for adj in self.adjacents():
-        #         adj.reveal()
 
     def both_release(self, *evt):
         adjs = list(self.adjacents())
@@ -369,10 +342,20 @@ class MapElem:
             for adj in adjs:
                 adj.reveal()
         else:
-            self.field.master.bell()
+            if self.field.master.var_sound.get():
+                self.field.master.bell()
 
-class MapIED(MapElem):
-    pass
+# class MapIED(MapElem):
+#     def reveal(self):
+#         # if self.field.map_cleared == 0
+#         if not self.revealed and not self.flagged:
+#             self.create_actual()
+#             self.box.destroy()
+#             self.revealed = True
+#             if not self.field.is_over:
+#                 self.lbl.config(fg='red', text='✨')
+#                 self.field.oops()
+#             return self.lbl
 
 
 class Surprise(tk.Button):
@@ -380,8 +363,8 @@ class Surprise(tk.Button):
         self.parent = parent
         super().__init__(
             master=parent.frame,
-            fg='red',
-            image=parent.field.empty_image,
+            fg='orange red',
+            image=parent.field.master.empty_image,
             width=20, height=20,
             compound='c',
             relief=tk.GROOVE,
@@ -402,6 +385,10 @@ class Surprise(tk.Button):
             self.flagged = True
             self.parent.field.IED_current.set(count - 1)
         return self.flagged
+
+    def check_false_flag(self):
+        if not self.parent.is_IED:
+            self.config(text='❌', fg='white', bg='maroon')
 
         
 if __name__ == '__main__':
